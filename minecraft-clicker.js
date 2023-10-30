efd32c4d8a02a898f8a7dc8cb879139fc928bc63
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2023-10-30 23:27:23 +0200

    added crafting resources

--- minecraft-clicker.js
+++ minecraft-clicker.js
@@ -3,20 +3,26 @@ const blocksMinedElement = document.getElementById("blocksMined");
 const resourcesElement = document.getElementById("resources");
 const mineButton = document.getElementById("mineButton");
 const upgradeButton = document.getElementById("upgradeButton");
+const craftButton = document.getElementById("craftButton");
+const craftedItemsElement = document.getElementById("craftedItems");
 
 // Constants
 const UPGRADE_COST = 10;
+const CRAFT_COST = 50; // Cost for crafting an item
 
 // Game state
 let blocksMined = 0;
 let resources = 0;
 let toolLevel = 1;
+let craftedItems = 0; // Number of crafted items
 
 // Function to update the UI
 function updateUI() {
     blocksMinedElement.textContent = blocksMined;
     resourcesElement.textContent = resources;
     upgradeButton.textContent = `Upgrade Tool (Cost: ${toolLevel * UPGRADE_COST} resources)`;
+    craftButton.textContent = `Craft Item (Cost: ${CRAFT_COST} resources)`;
+    craftedItemsElement.textContent = craftedItems;
 }
 
 // Event listener for mining
@@ -37,6 +43,18 @@ upgradeButton.addEventListener("click", () => {
         console.log(`Tool upgraded to level ${toolLevel}`);
     } else {
         console.log("Not enough resources to upgrade the tool.");
+    }
+});
+
+// Event listener for crafting
+craftButton.addEventListener("click", () => {
+    if (resources >= CRAFT_COST) {
+        resources -= CRAFT_COST;
+        craftedItems++;
+        updateUI();
+        console.log("Crafted an item!");
+    } else {
+        console.log("Not enough resources to craft an item.");
     }
 });
 
